59db31d8ac072b786635281f0f780631daaaac2e
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-22 17:55:15 -0500

    Add stlyes to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import { FcGlobe } from 'react-icons/fc';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 const Navbar = () => (
   <div className="NavBarContainer">
     <div className="Container_titleNavBar_Links">
       <h className="titleNavBar">B-Store ESSB</h>
       <div className="LinksContainer">
-        <Link className="Links_Navbar" to="/">Books</Link>
-        <Link className="Links_Navbar" to="/Categories">Categories</Link>
+        <Link className="Links_Navbar" to="/">BOOKS</Link>
+        <Link className="Links_Navbar" to="/Categories">CATEGORIES</Link>
       </div>
     </div>
     <a className="IconNavBar" target="_blank" href="https://github.com/ErikStoupignan" rel="noreferrer">
-      <FcGlobe className="IconNavBar App-logo" />
+      <BsFillPersonFill className="Icon_user" />
     </a>
   </div>
 );
