37137c13b75d6f9a201ad388a7ee2989b272e317
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-22 13:21:42 -0500

    Add style on category page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { FcGlobe } from 'react-icons/fc';
 
 const Navbar = () => (
   <div className="NavBarContainer">
-    <div>
-      <h1>Some Books</h1>
+    <div className="Container_titleNavBar_Links">
+      <h className="titleNavBar">B-Store ESSB</h>
+      <div className="LinksContainer">
+        <Link className="Links_Navbar" to="/">Books</Link>
+        <Link className="Links_Navbar" to="/Categories">Categories</Link>
+      </div>
     </div>
-    <div className="LinksContainer">
-      <Link className="bookLink" to="/">Books</Link>
-      <Link className="CategoriesLink" to="/Categories">Categories</Link>
-    </div>
+    <a className="IconNavBar" target="_blank" href="https://github.com/ErikStoupignan" rel="noreferrer">
+      <FcGlobe className="IconNavBar App-logo" />
+    </a>
   </div>
 );
 
